6699a5c4da046ce76c0592a77c1fc767e37dd625
sadegh1377 <sadeghhadipour1@gmail.com>
2021-05-15 17:49:13 +0430

    set font to vazirLight

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,33 @@
 
 <style>
 /*@import url('https://fonts.googleapis.com/css2?family=Markazi+Text&display=swap');*/
-@import url('https://fonts.googleapis.com/css2?family=Markazi+Text:wght@700&display=swap');
+/*@import url('https://fonts.googleapis.com/css2?family=Markazi+Text:wght@700&display=swap');*/
 /*@import url('https://fonts.googleapis.com/css2?family=Almarai:wght@800&display=swap');*/
 /*@import url('https://fonts.googleapis.com/css2?family=Almarai:wght@400&display=swap');*/
 /*@import url('https://fonts.googleapis.com/css2?family=Cairo:wght@600&display=swap');*/
+/*@font-face {*/
+/*  font-family: iranSans;*/
+/*  src: url("/src/assets/font/FontsFree-Net-ir_sans.ttf");*/
+/*}*/
+
+/*@font-face {*/
+/*  font-family: vazirLight;*/
+/*  src: url("/src/assets/font/FontsFree-Net-Vazir-Light.ttf");*/
+/*}*/
+@font-face {
+  font-family: "iranSans";
+  src: local("iranSans"),
+  url(assets/font/FontsFree-Net-ir_sans.ttf) format("truetype");
+}
+@font-face {
+  font-family: "vazirLight";
+  src: local("vazirLight"),
+  url(assets/font/FontsFree-Net-Vazir-Light.ttf) format("truetype");
+}
 
 #app {
-  font-family: Avenir, Helvetica, Arial, 'Markazi Text', 'Almarai', 'Cairo', sans-serif;
+  font-family: "vazirLight", Avenir, Helvetica, Arial, 'Markazi Text',
+  'Almarai', 'Cairo', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
